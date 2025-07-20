34745b9ff828510a0e4f21af87fe56414284c9f2
Nayan Jain <najain@griddynamics.com>
2025-07-21 00:48:49 +0530

    [Nayan Jain] adding common utility class

---
+++ src/main/java/com/griddynamics/storeapplication/util/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.griddynamics.storeapplication.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CommonUtils {
+
+  public static final String BASE_API = "/api";
+
+}
