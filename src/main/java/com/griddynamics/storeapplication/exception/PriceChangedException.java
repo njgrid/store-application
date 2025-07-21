83f528fdd2cd41b3566bac81fddf0931c443c165
Nayan Jain <najain@griddynamics.com>
2025-07-21 08:12:05 +0530

    [Nayan Jain] adding PriceChangedException

---
+++ src/main/java/com/griddynamics/storeapplication/exception/PriceChangedException.java
@@ -0,0 +1,9 @@
+package com.griddynamics.storeapplication.exception;
+
+public class PriceChangedException extends RuntimeException {
+
+  public PriceChangedException(final String message) {
+    super(message);
+  }
+
+}
