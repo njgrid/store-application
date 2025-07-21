ed0e004d6190821922fb56fb606c51e113e33bb8
Nayan Jain <najain@griddynamics.com>
2025-07-21 06:12:17 +0530

    [Nayan Jain] Adding custom exceptions handling logic

--- src/main/java/com/griddynamics/storeapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/griddynamics/storeapplication/exception/GlobalExceptionHandler.java
@@ -18,4 +18,50 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
   }
 
+  @ExceptionHandler(ProductDoesNotExistException.class)
+  public ResponseEntity<ErrorResponse> handleProductDoesNotExist(
+      final ProductDoesNotExistException productDoesNotExistException, final HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), productDoesNotExistException.getMessage(),
+        request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ItemAlreadyInCartException.class)
+  public ResponseEntity<ErrorResponse> handleItemAlreadyInCartException(
+      final ItemAlreadyInCartException itemAlreadyInCartException, final HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), itemAlreadyInCartException.getMessage(),
+        request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(InsufficientStockException.class)
+  public ResponseEntity<ErrorResponse> handleInsufficientStockException(
+      final InsufficientStockException insufficientStockException, final HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), insufficientStockException.getMessage(),
+        request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(ItemNotPresentInCartException.class)
+  public ResponseEntity<ErrorResponse> handleItemNotPresentInCartException(
+      final ItemNotPresentInCartException itemNotPresentInCartException, final HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), itemNotPresentInCartException.getMessage(),
+        request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+
+  @ExceptionHandler(CartNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleCartNotFoundException(final CartNotFoundException cartNotFoundException,
+      final HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), cartNotFoundException.getMessage(),
+        request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
 }
